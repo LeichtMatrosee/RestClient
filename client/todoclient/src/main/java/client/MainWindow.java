2abb93efbd56e18c1e92b38d1b068cb4fc7818c0
Gerrit Koppe <gkoppe@sievers-group.com>
2023-04-09 15:11:53 +0200

    Made dialog work

--- client/todoclient/src/main/java/client/MainWindow.java
+++ client/todoclient/src/main/java/client/MainWindow.java
@@ -1,6 +1,8 @@
 package client;
 
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.swing.JButton;
 
@@ -14,9 +16,12 @@ public class MainWindow extends TodoFrame {
 
     // Config
     String lastError;
+    ArrayList<HashMap<String, String>> lists;
+
 
     public MainWindow(String title) {
         super(title);
+        this.lists = new ArrayList<HashMap<String,String>>();
         this.loadAllLists = new JButton("Alle listen laden");
         this.loadAllLists.addActionListener(this);
         
@@ -50,7 +55,9 @@ public class MainWindow extends TodoFrame {
         if (e.getSource() == this.loadAllLists) {
             this.getAllLists();
         } else if (e.getSource() == this.addList) {
-            this.addList();
+            this.addLists();
+        } else if (e.getSource() == this.deleteList) {
+            this.deleteLists();
         }
     }
 
@@ -68,12 +75,20 @@ public class MainWindow extends TodoFrame {
         }
 
         for (int i = 0; i < rd.getEntries().length; i++) {
-            this.dlm.addElement(rd.getEntries()[i].getName());
+            this.lists.add(new HashMap<String,String>());
+            this.lists.get(i).put("name", rd.getEntries()[i].getName());
+            this.lists.get(i).put("id", rd.getEntries()[i].getId());
+            this.lists.get(i).put("description", rd.getEntries()[i].getDescription());
+        }
+
+        for (int i = 0; i < this.lists.size(); i++) {
+            this.dlm.addElement(this.lists.get(i).get("name"));
         }
         this.listScroller.setMaximum(rd.getEntries().length);
+        this.entries.setModel(this.dlm);
     }
 
-    private void addList() {
+    private void addLists() {
         AddWindow add = new AddWindow(this.frame, "Neue Liste");
 
         String name = add.getName();
@@ -82,9 +97,11 @@ public class MainWindow extends TodoFrame {
         ResponseData rd;
         if (!name.equals("")) {
             try {
-                rd = this.rc.addNewList(new PostData("list", name, description));
+                rd = super.rc.addNewList(new PostData("list", name, description));
             } catch (Exception e) {
-                this.lastError = e.getMessage();
+                if (!e.getMessage().equals("") || e.getMessage() != null) {
+                    this.lastError = e.getMessage();
+                }
                 return;
             }
         } else {
@@ -92,5 +109,23 @@ public class MainWindow extends TodoFrame {
         }
 
         this.dlm.addElement(rd.getEntries()[0].getName());
+        this.entries.setModel(this.dlm);
+    }
+
+    private void deleteLists() {
+        int index = this.entries.getSelectedIndex();
+        if (index == -1) return;
+
+        String idToDelete = this.lists.get(index).get("id");
+        try {
+            super.rc.deleteList(new PostData("list", "", "", idToDelete));
+        } catch (Exception e) {
+            if (!e.getMessage().equals("") || e.getMessage() != null) {
+                this.lastError = e.getMessage();
+            }
+            return;
+        }
+
+        this.getAllLists();
     }
 }
