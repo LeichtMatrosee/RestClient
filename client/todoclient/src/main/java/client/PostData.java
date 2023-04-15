86ccc0869cd4feba5e26fff52ee2d1b761043353
Gerrit Koppe <gkoppe@sievers-group.com>
2023-04-15 20:00:34 +0200

    Moved documentation and added comments to responsedata

--- client/todoclient/src/main/java/client/PostData.java
+++ client/todoclient/src/main/java/client/PostData.java
@@ -3,15 +3,49 @@ package client;
 import java.security.InvalidParameterException;
 import java.util.HashMap;
 
+/**
+ * Instances of this class encapsulate all information that is sent to the API.
+ * @author Gerrit Koppe
+ * @version 1.0
+ */
 public class PostData {
+    /**
+     * Type of the entity to be updated. Either "list" or "entry"
+     */
     private String type = "";
+    /**
+     * UUID of a todo-list. Must be set, when updating a list or doing anything
+     * entry-related.
+     */
     private String listId = "";
+    /**
+     * Name of the entity.
+     */
     private String name = "";
+    /**
+     * Description of the entity. Only useful for entries.
+     */
     private String description = "";
+    /**
+     * UUID of the entity.
+     */
     private String id = "";
+    /**
+     * @deprecated
+     */
     private boolean successful = false;
+    /**
+     * UUID of the entry.
+     */
     private String entryId = "";
 
+    /**
+     * Standard Constructor.
+     * Analyses map and initializes all instance fields with it's information.
+     * @param map Map containing all information for posting to API.
+     *  Valid attributes are: type, name, description, listId, entryId, id
+     *  All other attributes will be ignored.
+     */
     public PostData(HashMap<String,String> map) {
         if (!map.get("type").equals("list") && !map.get("type").equals("entry")) {
             throw new InvalidParameterException("Type must either be list or entry, was " + map.get("type") + " instead.");
@@ -26,48 +60,80 @@ public class PostData {
         if (map.get("id") != null) this.id = map.get("id");
     }
 
-    public PostData(String listId, String entryId, String name, String description, String type) throws InvalidParameterException {
-        if (type != "list" && type != "entry") {
-            throw new InvalidParameterException("Type must either be list or entry, was " + type + " instead.");
-        }
-        this.type = type;
-
-        this.name = name;
-        this.description = description;
-
-        if (listId == "" && type == "entry") {
-            throw new InvalidParameterException("ListId cannot be empty when pushing new entries for a list.");
-        }
-
-        if (entryId.equals("")) {
-            throw new InvalidParameterException("I made this constructor specifically for one endpoint, where you need an entry Id. "
-                + "Give me one or use a different constructor for crying out loud."
-            );
-        }
-        this.listId = listId;
-        this.entryId = entryId;
-    }
-
-
-    /**************** SETTERS AND GETTERS ****************/
+    /**
+     * Sets type of the entity described by the instance.
+     * @param type Either "list" or "entry"
+     */
     public void setType(String type) { this.type = type; }
+    /**
+     * Retrieves the type of the entity described by this instance.
+     * @return Type of the entity
+     */
     public String getType() { return this.type; }
     
+    /**
+     * Sets name of the entity described by the instance.
+     * @param name Name of the entity
+     */
     public void setName(String name) { this.name = name; }
+    /**
+     * Retrieves the name of the entity described by this instance.
+     * @return Name of the entity
+     */
     public String getName() { return this.name; }
 
+    /**
+     * Sets description of the entity described by the instance.
+     * @param description Description of the entity
+     */
     public void setDescription(String description) { this.description = description; }
+    /**
+     * Retrieves the description of the entity described by this instance.
+     * @return Description of the entity
+     */
     public String getDescription() { return this.description; }
 
+    /**
+     * Sets listId of the entity described by the instance.
+     * @param listId List ID of the entity
+     */
     public void setListId(String listId) { this.listId = listId; }
+    /**
+     * Retrieves the listId of the entity described by this instance.
+     * @return List UUID of the entity
+     */
     public String getListId() { return this.listId; }
 
+    /**
+     * Sets UUID of the entity described by the instance.
+     * @param id UUID of the entity
+     */
     public void setId(String id) { this.id = id; }
+    /**
+     * Retrieves the UUID of the entity described by this instance.
+     * @return UUID of the entity
+     */
     public String getId() { return this.id; }
 
+    /**
+     * @deprecated
+     * @param success
+     */
     public void setSuccessful(boolean success) { this.successful = success; }
+    /**
+     * @deprecated
+     * @return
+     */
     public boolean getSuccessful() { return this.successful; }
 
+    /**
+     * Sets EntryID of the entity described by the instance.
+     * @param entryId EntryId of the entity
+     */
     public void setEntryId(String entryId) { this.entryId = entryId; }
+    /**
+     * Retrieves the entryId of the entity described by this instance.
+     * @return Entry UUID of the entity
+     */
     public String getEntryId() { return this.entryId; }
 }
--- client/todoclient/src/main/java/client/ResponseData.java
+++ client/todoclient/src/main/java/client/ResponseData.java
@@ -4,46 +4,137 @@ import java.util.Map;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
+/**
+ * Class encapsulates the Response from the API in an easily and standardized manner.
+ * @author Gerrit Koppe
+ * @version 1.0
+ */
 public class ResponseData {
 
+    /**
+     * Class represents a single entry from the API Response
+     * @author Gerrit Koppe
+     * @version 1.0
+     */
     public class EntryData {
+        /**
+         * Name of the entry.
+         */
         private String name = "";
+        /**
+         * ID of the entry.
+         */
         private String id = "";
+        /**
+         * Description of the entry.
+         */
         private String description = "";
+        /**
+         * List ID of the entry.
+         */
         private String listId = "";
 
+        /**
+         * Default constructor.
+         */
         public EntryData() {}
 
+        /**
+         * Retrieves name of the entity.
+         * @return Name of the entity.
+         */
         public String getName() { return this.name; }
+        /**
+         * Sets the name of the entity.
+         * @param name Name of the enity
+         */
         public void setName(String name) { this.name = name; }
 
+        /**
+         * Retrieves name of the entity.
+         * @return Name of the entity.
+         */
         public String getId() { return this.id; }
+        /**
+         * Sets the UUID of the entity.
+         * @param id UUID of the entity
+         */
         public void setId(String id) { this.id = id; }
 
+        /**
+         * Retrieves name of the entity. Only useful if entity is an entry and not a list.
+         * @return Name of the entity.
+         */
         public String getDescription() { return this.description; }
+        /**
+         * Sets the description of the entity. Only useful if entity is an entry and not a list.
+         * @param description Description of the entity.
+         */
         public void setDescription(String description) { this.description = description; }
 
+        /**
+         * Retrieves List UUID of the entity. Only useful if entity is an entry and not a list.
+         * @return List UUID of the entity.
+         */
         public String getListId() { return this.listId; }
+        /**
+         * Sets the list UUID of the entity. Only useful if entity is an entry and not a list.
+         * @param listId List UUID of the entity.
+         */
         public void setListId(String listId) { this.listId = listId; }
 
     }
     
+    /**
+     * Array of entities from API Response.
+     */
     private EntryData[] entries;
+    /**
+     * Shows, how many entities were written, as declared in API response.
+     */
     private int written;
+
+    /**
+     * @deprecated
+     */
     private int total;
 
-    // Delete param
+    /**
+     * Shows, how many entities were deleted, as declared in API response.
+     */
     private int deleted;
 
-    // Update Param
+    /**
+     * Shows, how many entities were updated, as declared in API response.
+     */
     private int entriesUpdated;
 
+    /**
+     * @deprecated
+     * Shows, whether API request was successful.
+     */
     private boolean success;
+    /**
+     * Message from API Response
+     */
     private String message;
 
+    /**
+     * Status Code of the API Request.
+     */
     private int statusCode;
+
+    private String exceptionType;
     
+    /**
+     * Primary Constructor for ResponseData object.
+     * Map is the API response parsed into a easily accessable object.
+     * @see client.RestCommunicator.sendHttpRequest for more information on parsing the map.
+     * @param map Parsed API Response. 
+     * @param statusCode Status code of the http response.
+     */
     public ResponseData (Map<String,Object> map, int statusCode) {
+        // If the api responded with entries, iterate through all of them and instanciate EntryData objects for every one.
         if (map.get("entries") != null) {
             ArrayList<LinkedHashMap<String,String>> rEntries = (ArrayList<LinkedHashMap<String,String>>) map.get("entries");
             this.entries = new EntryData[rEntries.size()];
@@ -58,104 +149,168 @@ public class ResponseData {
             }
         }
 
+        // Set the statuscode
         this.statusCode = statusCode;
 
+        // Check every key and set the corresponding instance field
         if (map.get("deleted") != null) {
             this.deleted = (int) map.get("deleted");
-        }
+        } else this.deleted = -1;
 
         if (map.get("written") != null) {
             this.written = (int) map.get("written");
-        }
+        } else this.written = -1;
 
         if (map.get("total") != null) {
             this.total = (int) map.get("total");
-        }
+        } else this.total = -1;
 
         if (map.get("entries_updated") != null) {
             this.entriesUpdated = (int) map.get("entries_updated");
-        }
-
-        this.success = true;
+        } else this.entriesUpdated = -1;
     }
 
+    /**
+     * Secondary constructor. If API request resulted in an exception, the Class can still be instanciated
+     * with the information of the Exception.
+     * Message of the exception will be saved in class field {@link #message}.
+     * Type of exception (class) will be saved in class field {@link #exceptionType}.
+     * @param e Exception that occured during API Request 
+     */
     public ResponseData(Exception e) {
-        this.success = false;
-
         if (e.getMessage() != null) {
             this.message = e.getMessage();
         } else {
             this.message = "No further cause for exception found.";
         }
+
+        this.exceptionType = e.getClass().toString();
     }
-
-    /*************** SETTERS AND GETTERS ***********************/
     
-
+    /**
+     * Retrieves all entries sent by the API Response.
+     * @return Entries sent by the API Response.
+     */
     public EntryData[] getEntries() {
         return this.entries;
     }
 
+    /**
+     * Sets the Entries
+     * @param entries All Entries
+     */
     public void setEntries(EntryData[] entries) {
         this.entries = entries;
     }
 
+    /**
+     * Retrieves how many entries were written as declared by the API Response.
+     * @return How many entries were written in API request.
+     */
     public int getWritten() {
         return this.written;
     }
 
+    /**
+     * Set how many entries were written
+     * @param written How many entries were written
+     */
     public void setWritten(int written) {
         this.written = written;
     }
 
+    /**
+     * @deprecated
+     * @return total
+     */
     public int getTotal() {
         return this.total;
     }
 
+    /**
+     * @deprecated
+     * @param total
+     */
     public void setTotal(int total) {
         this.total = total;
     }
 
+    /**
+     * Retrieves how many entries were deleted as declared by the API Response.
+     * @return How many entries were deleted in API request.
+     */
     public int getDeleted() {
         return this.deleted;
     }
 
+    /**
+     * Set how many entries were deleted in request
+     * @param deleted How many entries were deleted in request
+     */
     public void setDeleted(int deleted) {
         this.deleted = deleted;
     }
 
+    /**
+     * Retrieves how many entries were updated as declared by the API Response.
+     * @return How many entries were updated in API request.
+     */
     public int getEntriesUpdated() {
         return this.entriesUpdated;
     }
 
+    /**
+     * Set how many entries were updated in request
+     * @param deleted How many entries were updated in request
+     */
     public void setEntriesUpdated(int entriesUpdated) {
         this.entriesUpdated = entriesUpdated;
     }
 
-    public boolean isSuccess() {
-        return this.success;
-    }
-
+    /**
+     * @deprecated
+     * @return isSuccess
+     */
     public boolean getSuccess() {
         return this.success;
     }
 
+    /**
+     * @deprecated
+     * @param success
+     */
     public void setSuccess(boolean success) {
         this.success = success;
     }
 
+    /**
+     * Gets the message of the API response.
+     * @return Message of the API response.
+     */
     public String getMessage() {
         return this.message;
     }
 
+    /**
+     * Set message of api response
+     * @param deleted message of api response
+     */
     public void setMessage(String message) {
         this.message = message;
     }
 
+    /**
+     * Retrieves status code of the api response.
+     * @return Status code of the API Response.
+     */
     public int getStatusCode() {
         return this.statusCode;
     }
 
+    /**
+     * Set status code of api response
+     * @param statusCode status code of api response.
+     */
     public void setStatusCode(int statusCode) {
         this.statusCode = statusCode;
     }
